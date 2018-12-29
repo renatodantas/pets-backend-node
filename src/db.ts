b747c0b77c47d55c4fde54dcbcde6971d9fc01f1
Renato Dantas <rexdantas@gmail.com>
2018-12-29 12:05:09 -0200

    Inclusão de Typescript parcialmente concluída
    Por algum motivo o Express não está roteando para as subrotas raiz e pets.

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,9 @@
 // Reference: https://node-postgres.com/features/connecting
 
 import pg from 'pg'
-import sqorn from '@sqorn/pg'
 
 export function test() {
   const pool = new pg.Pool()
-  const sq = sqorn({ pg, pool })
-  
-  const donos = sq.from`tb_dono`.query
-  console.log(donos)
+  console.log('Testando conexão do banco!')
+  console.log(pool)
 }
